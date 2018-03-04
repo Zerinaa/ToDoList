34e7af52c30ad30bbf0096d7d87da9f02943e95d
Zerinaa <37039753+Zerinaa@users.noreply.github.com>
2018-03-04 23:26:43 +0100

    Add files via upload

---
+++ todoList.js
@@ -0,0 +1,37 @@
+
+$("form").submit(function(e) {
+  var red = "";
+  red += "<tr>";
+  red += "<td class='col-xs-6' id='txt-input'>";
+  red += $("#txt-input").val();
+  red += "</td>";
+  red += "<td class='col-xs-1 columns'>";
+  red += "<input type='checkbox' id='check' class='checkbox-size'>";
+  red += "</td>";
+  red += "<td class='col-xs-1 columns'>";
+  red += "<img src='https://i.imgur.com/Dnl2hhP.png' height='20' id='deleted'>";
+  red += "</td>";
+  red += "</tr>";
+  
+  $("tbody").append(red);
+  $("#txt-input").val("");
+  e.preventDefault();
+});
+
+$("table").on("click", "#check", function() {
+  if($(this).is(":checked")) {
+    $(this).parent().parent().addClass("green");
+  }
+  else {
+    $(this).parent().parent().removeClass("green");
+  }
+});
+
+$("table").on("click","#deleted",function() {
+  $(this).parent().parent().remove();
+});
+
+
+
+
+
